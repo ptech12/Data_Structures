7fb27629feba28f1a86720c536c9a39b9e6f9859
Prasanth A R <raviprasanth45@gmail.com>
2021-02-19 17:34:59 +0530

    Initital Linked List working

--- C++/header/linkedlist.h
+++ C++/header/linkedlist.h
@@ -10,16 +10,15 @@ private:
     {
         private:
             T data;
-            T *next;
+            Node *next;
         public:
             friend class LinkedList; 
     };
     Node *head = NULL; 
-
+    Node *list[10] = NULL;
 public:
     LinkedList();
     friend class Node; 
-    ~LinkedList();
 public:
     T createMemory();   
     bool insertFirst(T data);
@@ -28,36 +27,35 @@ public:
 template <class T>
 LinkedList<T>::LinkedList()
 {
-    std::cout << "Node created and attached to head successfully" << endl;
-    head = Node();
+    std::cout << "Node created and attached to head successfully" << std::endl;
+    head = new Node();
+    list = new Node();
 }
 template <class T>
 T LinkedList<T>::createMemory()
 {
     return new Node();
 }
-// template <class T>
-// bool LinkedList<T>::insertFirst(T data)
-// {
+template <class T>
+bool LinkedList<T>::insertFirst(T data)
+{
 
-//     if (head == NULL)
-//     {
-//         return false;  
-//     }
-//     else if(head != NULL)
-//     {
-//         T *temp = createMemory();
-//         temp = head;
-//         while (temp != NULL)
-//         {
-//             temp = temp->next;
-//         }
-//         list->next = temp;
+    if (head == NULL)
+    {
+        return false;  
+    }
+    else if(head != NULL)
+    {
+        T *temp = createMemory();
+        temp = head;
+        while (temp != NULL)
+        {
+            temp = temp->next;
+        }
+        list->next = temp;
                 
-//     }
-//     else{
-//         head = data;
-//     }
-
-    
-// }
+    }
+    else{
+        head = data;
+    }   
+}
